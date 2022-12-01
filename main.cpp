1992eccd3d2df13086c0e7f1b21c96999152c875
JAZMYNW <95877548+JAZMYNW@users.noreply.github.com>
2022-11-30 19:35:30 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Graph.h"
+
+int main(int argc, const char * argv[]) {
+    
+    if (argc != 2)
+    {
+        cout << "Please supply a file name as input" << endl;
+        return -1;
+    }
+    
+    Graph graph;
+    graph.generateGraph(argv[1]);
+    graph.printGraph();
+    graph.lowestReachable();
+    
+    return 0;
+}
